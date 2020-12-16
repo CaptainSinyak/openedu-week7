8e51774298181c1a5317297b648d92da9a5b2400
CaptainSinyak <antonioe15000@gmail.com>
2020-12-16 15:18:33 +0300

    commit on master

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ export default (express, bodyParser, fs, crypto, http, mongodb, path, cors) => {
     app.options('*', cors());
 
     app 
-        .get('/wordpress/wp-json/wp/v2/posts/1', (req, res) => res.status(200).json({title: {id: 1, rendered: "itmo282167"}}))
+        .get('/wordpress/wp-json/wp/v2/posts/1', (req, res) => res.status(200).json({title: {id: 1, rendered: "itmo288485"}}))
         .post('/render/', (req, res) => {
             const {random2, random3} = req.body;
 
@@ -71,7 +71,7 @@ export default (express, bodyParser, fs, crypto, http, mongodb, path, cors) => {
             res.status(200).end();
         
         })
-        .get('/login/', (req, res) => res.send('itmo282167'))
+        .get('/login/', (req, res) => res.send('itmo288485'))
         .get('/code/', (req, res) => fs.createReadStream(import.meta.url.substring(7)).pipe(res))
         .get('/sha1/:input/', (req, res) => {
             const { input } = req.params;
@@ -124,7 +124,7 @@ export default (express, bodyParser, fs, crypto, http, mongodb, path, cors) => {
             });
         })
         .all('*', (req, res) => {
-            res.send('itmo282167');
+            res.send('itmo288485');
         });
 
 
